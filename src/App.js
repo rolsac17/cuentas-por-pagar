289991aa64f8e3414f9eccbb2adbbae3c6efba46
JEstivenA <estiven6647@gmail.com>
2022-10-19 23:50:15 -0600

    Creacion de menu

--- src/App.js
+++ src/App.js
@@ -1,80 +1,37 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
-import './App.css';
 
-function Login() {
+import Login from './Components/Logins';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import {Routes} from 'react-router-dom';
+import Menu from './Components/Menu';
+
+
+
+function App() {
   return (
-    <div className="App">
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-25 w-25"
-              src="https://images.vexels.com/media/users/3/224169/isolated/lists/dbfe1f493ad01117fa4ec5ba10150e4d-logotipo-de-programaci-n-inform-tica.png"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Inicia con tu cuenta administrativa
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600"> </p>
+
+    <Router>
+
+      <Routes>
+
+        <Route path='/' element={<Login />}/>
+        <Route path='/menu' element={<Menu />}/>
+          {/* <div className='app'>
+          <Login />
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Usuario"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Contraseña"
-                />
-              </div>
-            </div>
+        </Route> */}
 
-            <div className="flex items-center justify-between">
+      </Routes>
 
-              <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Olvidaste tu contraseña?, contacta al equipo de soporte
-                </a>
-              </div>
-            </div>
+      
 
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Entrar
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    </Router>
+
+    
   );
 }
 
-export default Login;
+export default App;
