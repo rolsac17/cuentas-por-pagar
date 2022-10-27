9477b6d34c2a0581cc91fb1c2ddccd12b7e5d64b
Eduardo-Collado <eduardojose5coc@gmail.com>
2022-10-26 20:09:22 -0600

    1st Apis Use

--- src/Components/Logins.js
+++ src/Components/Logins.js
@@ -96,7 +96,7 @@ class Login extends React.Component {
                 </div>
     
                 <div>
-                  <Link to={"/menu"}
+                  <Link to={"/historial"}
                     onClick={this.manejadorBoton}
                     type="submit"
                     className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#701a75] py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
